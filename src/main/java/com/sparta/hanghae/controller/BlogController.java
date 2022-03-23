9ba0d17f9864dd981c6bdac05c30be8974d4402f
codegyeon <wjdrlgus313@naver.com>
2022-03-23 23:09:24 +0900

    2번

--- src/main/java/com/sparta/hanghae/controller/BlogController.java
+++ src/main/java/com/sparta/hanghae/controller/BlogController.java
@@ -6,34 +6,86 @@ import com.sparta.hanghae.domain.BlogRequestDto;
 import com.sparta.hanghae.service.BlogService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class BlogController {
     private final BlogRepository blogRepository;
     private final BlogService blogService;
 
+//    @GetMapping(value =  "/api/blog")
+//    public String main (){
+//        return "index";
+//    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+    @GetMapping("postpage")
+    public String postpage() {
+        return "postpage";
+    }
+    @GetMapping("detail/{id}")
+    public String detailpage(@PathVariable Long id, Model model) {
+        blogRepository.findById(id).orElse(null);
+        model.addAttribute("details",blogRepository.findById(id).orElse(null));
+        return "detail";
+    }
+//    @GetMapping ("/api/blog/postpage")
+//    public String postpage (Model model){
+//        model.addAttribute("name", model);
+//        return "postpage";
+//    }
+
     @PostMapping("/api/blog/postpage")
+    @ResponseBody
     public Blog createBlog(@RequestBody BlogRequestDto requestDto) {
         Blog blog = new Blog(requestDto);
         return blogRepository.save(blog);
     }
 
     @GetMapping("/api/blog")
+    @ResponseBody
     public List<Blog> getBlog() {
         return blogRepository.findAllByOrderByModifiedAtDesc();
     }
+//    @GetMapping("detail/{id}")
+//    public ModelAndView findBoard(@PathVariable Long id){
+//        ModelAndView mv = new ModelAndView();
+//
+//        mv.setViewName("detail");
+//        return mv;
+//    }
+//    @GetMapping( "detail/{id}")
+//    @ResponseBody
+//    public Blog getBlogdetail(@PathVariable Long id) {
+//
+//        return blogRepository.findById(id).orElse(null);
+//    }
+//    @GetMapping("detail/{id}")
+//    @ResponseBody
+//    public ModelAndView findBoard(@PathVariable Long id){
+//        ModelAndView mv = new ModelAndView();
+//        Blog blog = blogRepository.findById(id).orElseThrow( () -> new NullPointerException("게시물이 없습니다."));
+//        mv.setViewName("detail");
+//        return mv;
+//    }
 
-    @DeleteMapping("/api/memos/{id}")
+    @DeleteMapping("/api/delete/{id}")
+    @ResponseBody
     public Long deleteBlog(@PathVariable Long id) {
         blogRepository.deleteById(id);
         return id;
     }
 
-    @PutMapping("/api/memos/{id}")
+    @PutMapping("/api/blog/postpage/{id}")
     public Long updataBlog(@PathVariable Long id, @RequestBody BlogRequestDto requestDto) {
         blogService.update(id, requestDto);
         return id;
---
+++ src/main/java/com/sparta/hanghae/controller/DlogController.java
@@ -0,0 +1,30 @@
+package com.sparta.hanghae.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RequiredArgsConstructor
+@Controller
+public class DlogController {
+//    @RequestMapping(value =  "/")
+//    public String main (){
+//        return "index";
+//    }
+//
+//    @RequestMapping ("/postpage")
+//    public String postpage (){
+//
+//        return "redirect:/postpage";
+//    }
+
+//    @Controller
+//    public class HomeController{
+//        @GetMapping("/api/blogs/detail")
+//        public String detail(@RequestParam("id") Long id) {
+//            return "/detail.html";
+//        }
+}
